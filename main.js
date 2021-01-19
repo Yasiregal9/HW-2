388af1d4caead883a23a6fd8c70293d3411ed566
Yasir Egal <yasir.egal@bellevuecollege.edu>
2021-01-19 15:58:28 -0800

    Homework2

--- main.js
+++ main.js
@@ -1,41 +1,46 @@
-// document.addEventListener("DOMContentLoaded", function (event) {
-
-//     document.getElementById("button1").addEventListener("click", function () {
-
-//         document.getElementById("textbox1").value = "success";
-//     });
-// });
-
-let startBtn = document.querySelector("#start");
-let swapBtn = document.querySelector("#swap");
 
 
-startBtn.addEventListener("click",LoopintoArray,false);
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("swap").style.visibility = "hidden";
+    document.getElementById("swappedScreen").style.display = "none";
+    document.getElementById("start").addEventListener("click", LoopintoArray, false) 
+    document.getElementById("swap").addEventListener("click", swapLetters, false) 
 
-for(i=0;i<3;i++)
-{
-    swapBtn.addEventListener("click",swappedletters(arr[i]),false);    
-}
+});
 
-let secondArray = arr.map(
-   
-    function swappedletters(oneWord)
- {      
-    return oneWord.charAt(oneWord.length-1)+oneWord.substring(1,oneWord.length-1)+oneWord.charAt(0);    
- }
-);
+let arr1 = [];
 
 function LoopintoArray()
 {
     for(let i=0;i<3;i++)
     {
-        arr.push(prompt("Enter words: "));
+        arr1[i]=(prompt("Enter words: "));
+        let anotherLi = document.createElement("li");
+        anotherLi.appendChild(document.createTextNode(arr1[i]));
+        document.getElementById("firstUl").appendChild(anotherLi);
     }
+    document.getElementById("swap").style.visibility = "visible";
+    document.getElementById("start").style.visibility = "hidden";
 
-    // for(1=0;i<3;i++)
-    // {
-    //     console.log(secondArray[i]);
-    // }
+
+
+}
+
+function swapLetters()
+{
+    document.getElementById("swappedScreen").style.display = "block";
+    document.getElementById("normalScreen").style.display = "none";
+
+    let secondArray = arr1.map(function(oneWord) {       
+     return oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0);
+     });
+
+    for(let i=0;i<3;i++)
+    {
+        let anotherLi = document.createElement("li");
+        anotherLi.appendChild(document.createTextNode(secondArray[i]));
+        document.getElementById("secondUl").appendChild(anotherLi);
+    }
 }
 
 
